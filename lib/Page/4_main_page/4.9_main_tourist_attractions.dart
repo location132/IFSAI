89e42c9cc33f2eddfecfc341f5e3bd6bb27ab82f
Ljw <132702102+location132@users.noreply.github.com>
2024-06-27 14:48:56 +0900

    fix: 메인화면 서버 사진 + 텍스트 추가 (명소)
    1차 수정중이며,
    2차 수정에서 생명주기 (유지) , 오버플로우가 수정되서 올라갈 예정입니다.

--- lib/Page/4_main_page/4.9_main_tourist_attractions.dart
+++ lib/Page/4_main_page/4.9_main_tourist_attractions.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:my_dream/Page/4_main_page/4.9.1_main_tourist_container.dart';
+import 'package:my_dream/Page/4_main_page/main_dio/main_screen_dio.dart';
+
+
 class MainTouristAttractions extends StatefulWidget {
   const MainTouristAttractions({super.key});
 
@@ -8,54 +12,35 @@ class MainTouristAttractions extends StatefulWidget {
 }
 
 class _MainTouristAttractionsState extends State<MainTouristAttractions> {
-  Widget buildAttractionCard() {
-    final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
 
-    return Container(
-      height: screenHeight * 0.282,
-      width: screenWidth * 0.44,
-      decoration: BoxDecoration(
-        color: Colors.grey,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: const Stack(
-        children: [
-          Positioned(
-            left: 10,
-            bottom: 10,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      '#부산 #관광지',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 12,
-                        fontFamily: 'Pretendard',
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 5),
-                Text(
-                  '스카이캡슐 \n반값으로 입장하기',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontFamily: 'Pretendard',
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+  List<Map<String, dynamic>> serverResult = [];
+  Map<String, dynamic> storePlace1 = {};
+  Map<String, dynamic> storePlace2 = {};
+  Map<String, dynamic> storePlace3 = {};
+  Map<String, dynamic> storePlace4 = {};
+
+  bool _isFinish = false;
+
+  @override
+  void initState() {
+    super.initState();
+    attractionsGetDio();
+  }
+
+  void attractionsGetDio() async {
+    serverResult = await mainScreenTourism();
+
+    _isFinish = dataProcessing();
+    setState(() {});
+  }
+
+  bool dataProcessing() {
+    storePlace1 = serverResult[0];
+    storePlace2 = serverResult[1];
+    storePlace3 = serverResult[2];
+    storePlace4 = serverResult[3];
+    return true;
+
   }
 
   @override
@@ -94,20 +79,90 @@ class _MainTouristAttractionsState extends State<MainTouristAttractions> {
         const SizedBox(height: 10),
         Row(
           children: [
-            buildAttractionCard(),
+
+            BuildAttractionCard(
+                serverResultData: _isFinish ? serverResult[0] : {}),
             const SizedBox(width: 14), // 간격 추가
-            buildAttractionCard(),
+            BuildAttractionCard(
+                serverResultData: _isFinish ? serverResult[1] : {}),
+
           ],
         ),
         const SizedBox(height: 14),
         Row(
           children: [
-            buildAttractionCard(),
+
+            BuildAttractionCard(
+                serverResultData: _isFinish ? serverResult[2] : {}),
             const SizedBox(width: 14), // 간격 추가
-            buildAttractionCard(),
+            BuildAttractionCard(
+                serverResultData: _isFinish ? serverResult[3] : {}),
           ],
         ),
       ],
     );
   }
 }
+
+// class buildAttractionCard extends StatelessWidget {
+//   const buildAttractionCard({
+//     super.key,
+//     required this.context,
+//   });
+
+//   final BuildContext context;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final screenWidth = MediaQuery.of(context).size.width;
+//     final screenHeight = MediaQuery.of(context).size.height;
+
+//     return Container(
+//       height: screenHeight * 0.282,
+//       width: screenWidth * 0.44,
+//       decoration: BoxDecoration(
+//         color: Colors.grey,
+//         borderRadius: BorderRadius.circular(8),
+//       ),
+//       child: ClipRRect(
+//         borderRadius: BorderRadius.circular(8),
+//         child: const Stack(
+//           children: [
+//             Positioned(
+//               left: 10,
+//               bottom: 10,
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   Row(
+//                     children: [
+//                       Text(
+//                         '#부산 #관광지',
+//                         style: TextStyle(
+//                           color: Colors.white,
+//                           fontSize: 12,
+//                           fontFamily: 'Pretendard',
+//                           fontWeight: FontWeight.w600,
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                   SizedBox(height: 5),
+//                   Text(
+//                     '스카이캡슐 \n반값으로 입장하기',
+//                     style: TextStyle(
+//                       color: Colors.white,
+//                       fontSize: 16,
+//                       fontFamily: 'Pretendard',
+//                       fontWeight: FontWeight.w600,
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
