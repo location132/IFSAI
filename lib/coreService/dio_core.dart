b884a5a5850f02d81c4587a01bc8c1030b167297
Ljw <132702102+location132@users.noreply.github.com>
2025-06-25 20:57:40 +0900

    Delete lib directory

--- lib/coreService/dio_core.dart
+++
@@ -1,9 +0,0 @@
-import 'package:my_dream/Page/2_login_page/login_dio/login_dio.dart';
-
-// 토큰 재발급
-Future<void> dioCore(var message) async {
-  await storage.write(
-    key: 'accessToken',
-    value: message['data']['accessToken'],
-  );
-}
